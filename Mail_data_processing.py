4422aeacfeaab2e1210e4a16ada14f5150523cc6
João Barboza Rodrigues <joaobarboza.ufu@gmail.com>
2020-09-26 15:51:29 -0300

    Update Mail_data_processing.py

--- Mail_data_processing.py
+++ Mail_data_processing.py
@@ -1,1 +1,3 @@
 # Code beeing developed that reads a csv file containing all zip codes and creates a database containing these data
+my_file = open("book1.txt", "r" , encoding="utf8")
+yourResult = [line.split(';') for line in my_file.readlines()]
